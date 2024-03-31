31339e1cfe2129a22d09a27d26297e24274ecd2c
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-03-31 12:54:36 -0400

    New relationship able to retrive relationshiops via the user or service or town

--- backend/models.py
+++ backend/models.py
@@ -20,28 +20,47 @@ class UserServiceAssoc(BaseModel, Base):
         lazy=joined allows to load the objects from the relationships in ...
         same time you query from 'user_service_assoc'
     """
-    user = relationship('User', cascade='all, delete', lazy='joined')
-    town = relationship('Town', cascade='all, delete', lazy='joined')
-    service = relationship('Service', cascade='all, delete', lazy='joined')
+    user = relationship('User', cascade='all, delete', lazy='joined', back_populates='user_service_assoc')
+    town = relationship('Town', cascade='all, delete', lazy='joined', back_populates='user_service_assoc')
+    service = relationship('Service', cascade='all, delete', lazy='joined', back_populates='user_service_assoc')
 
-    __table_args__ = (
-        UniqueConstraint('user_id', 'service_id', 'town_id'),
-    )
+    __table_args__ = (UniqueConstraint('user_id', 'service_id', 'town_id'),)
+
+class CategoryServiceAssoc(BaseModel, Base):
+    __tablename__ = 'categories_service_assoc'
+
+    category_id = Column(String, ForeignKey('categories.id', ondelete='CASCADE'), nullable=False)
+    service_id = Column(String, ForeignKey('services.id', ondelete='CASCADE'), nullable=False)
+
+    category = relationship('Category', cascade='all, delete', lazy='joined')
+    service = relationship('Service', cascade='all, delete', lazy='joined')
 
 class Service(BaseModel, Base):
     __tablename__ = 'services'
 
-    name = Column(String(50), nullable=False)
+    name = Column(String(50), unique=True, nullable=False)
+
+    user_service_assoc = relationship('UserServiceAssoc', back_populates='service')
 
 class User(BaseModel, Base):
     __tablename__ = 'users'
 
     first_name = Column(String(255), nullable=False)
     last_name = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
 
+    user_service_assoc = relationship('UserServiceAssoc', back_populates='user')
 
 
 class Town(BaseModel, Base):
     __tablename__ = 'towns'
 
-    name = Column(String(50), nullable=False)
+    name = Column(String(50), unique=True, nullable=False)
+
+    user_service_assoc = relationship('UserServiceAssoc', back_populates='town')
+
+class Category(BaseModel, Base):
+    __tablename__ = 'categories'
+
+    name = Column(String(50), unique=True, nullable=False)
+
