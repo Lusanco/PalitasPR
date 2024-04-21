c7e6aa4e08a5f36da4ef41b08846cb3bdf97b586
Ltoro9 <louistororosario@gmail.com>
2024-04-21 17:31:53 -0400

    RUNNING VERSION

--- backend/app.py
+++ backend/app.py
@@ -25,61 +25,6 @@ Base.metadata.bind = engine
 def index():
     return render_template('index.html')
 
-@app.route('/users')  # API??? FRONTEND CAN CALL THIS
-def all_users():
-    """
-        Return all Users in DB to frontend
-
-        This function retrieves all users from the database and returns them to the frontend.
-        It accepts optional query parameters to filter the users. If no parameters are provided,
-        it fetches all users. It then serializes the user objects and returns them as JSON.
-        Ensure that request and jsonify are imported from Flask.
-    """
-
-    query_params = request.args
-
-    if query_params:
-        # Filter users based on query parameters
-        users = DBOperations().filter('User', **query_params)
-        serialized_users = users
-    else:
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        
-        users = session.query(User).all()
-
-        # Retrieve all the columns for each object and the values
-        serialized_users = [user.all_columns() for user in users]
-
-        # Close the session to release resources
-        session.close()
-    
-    return jsonify(serialized_users)
-
-@app.route('/services') # API??? FRONTEND CAN CALL THIS
-def all_services():
-    """
-        Return all services in DB to frontend
-    """
-    query_params = request.args
-
-    if query_params:
-        # Filter services based on query parameters
-        services = DBOperations().filter('Service', **query_params)
-        serialized_services = services
-    else:
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        
-        services = session.query(Service).all()
-
-        # Retrieve all the columns for each object and the values
-        serialized_services = [service.all_columns() for service in services]
-        
-        # Close the session to release resources
-        session.close()
-    
-    return jsonify(serialized_services)
 
 @app.route('/create_object', methods=['POST'])
 def create_object():
