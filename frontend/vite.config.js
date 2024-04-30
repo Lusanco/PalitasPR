809e2bcb6cf8c7994b14d39ebb2bba52ca065330
lusanco <lasc1026@gmail.com>
2024-04-29 22:59:29 -0400

    WIP - some progress with autoscrolls, scrollbars and scrolling behaviour throughout the app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,12 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      // Replace with your Flask app's port
+      // "/api": "http://localhost:5000",
+      "/api": "http://127.0.0.1:5000",
+    },
+  },
   plugins: [svelte()],
 });
