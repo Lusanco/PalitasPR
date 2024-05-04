5bab2c8ddf129d57e0edaf5cb232131bbd130199
Ltoro9 <louistororosario@gmail.com>
2024-05-04 11:36:26 -0400

    add main and api blueprints

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from sqlalchemy import create_engine
 from base_model import Base
-from db_operations import DBOperations  # Import the method for creating new objects
-from routes import app_bp
+from db_operations import DBOperations
+from blueprints import main_bp
+from api_blueprint import api_bp
 from flask_mail import Mail
-from emails import confirm_email
+
+
 
 
 # Create Flask app instance
@@ -19,38 +21,21 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = "antoniofdjs@gmail.com"
 app.config["MAIL_PASSWORD"] = "syhk sijd eoli tgba"
-app.register_blueprint(app_bp)
+app.register_blueprint(main_bp)
+app.register_blueprint(api_bp, url_prefix='/api')
 mail = Mail(app)
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+
 CORS(app)
 
-# Create the engine
 engine = create_engine('postgresql://postgres:9495@localhost/postgres')
 
-# Bind the engine to the Base class
 Base.metadata.bind = engine
 
-# VERIFY EMAIL ROUTE
-@app.route("/verify_email/<token>", methods=["GET"])
-def verify_email(token):
-
-    if not token:
-        return "Verification token is missing"
-
-    verified = confirm_email(token)
-
-    if not verified:
-        return "Invalid verification token"
-
-    return "Email verification successful"
 
 
 @app.route("/")
 def index():
-    # Serve the built index.html from the frontend directory
-    # return serve_static("index.html")
     return app.send_static_file("index.html")
 
 
@@ -64,101 +49,5 @@ def serve_static(filename):
         return "File not found", 404
 
 
-@app.route("/login")
-def login_page():
-    return render_template("login.html")
-
-
-@app.route("/signup")
-def signup_page():
-    return render_template("signup.html")
-
-
-@app.route("/login", methods=["POST"])
-def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    DBOperations().login(email, password)
-    response_html = render_template("login_response.html")
-    return response_html
-
-@app.route('/api/explore', methods=['GET'])
-def explore():
-    model = request.args.get('model')
-    service = request.args.get('search')
-    town = request.args.get('town')
-    print("AFTER GET ARGS REQUESTS")
-    print(model)
-    print(service)
-    print(town)
-    search_results = DBOperations().filter(model, service, town)
-    if search_results:
-        return jsonify(search_results)
-    else:
-        return jsonify("No Results"), 404
-
-@app.route("/api/create_object", methods=["POST"])
-def create_object():
-    form_data = request.get_json()
-
-    # If user is signing up
-    if (
-        "first_name" in form_data
-        and "last_name" in form_data
-        and "email" in form_data
-        and "password" in form_data
-    ):
-        user_data = {
-            "first_name": form_data["first_name"],
-            "last_name": form_data["last_name"],
-            "email": form_data["email"],
-            "password": form_data["password"],
-        }
-        new_obj = DBOperations().sign_up(user_data)
-    else:
-        new_obj = DBOperations().new(form_data)
-
-    # Check if object was created
-    if new_obj:
-        return {"response": "success"}
-    else:
-        pass
-        # return render_template("error.html", error="Error creating object"), 400
-    return
-
-
-@app.route("/api/filter", methods=["POST"])
-def search_filter():
-    """
-    Front has to send {'Service': {'name': 'Nails, 'town': 'Ponce'}}
-    town is an optional argument for dict
-
-    Usage:  {'object_id': {'parameter1': 'value1', 'parameter2': 'value2'}}
-
-    example:
-        filtered_objs = db.filter({'User': {'name': 'service_name', 'town': 'town_name'}})
-    """
-    data = request.json
-    dictionary = DBOperations().filter(data)
-    print(data)
-    print(1, dictionary)
-
-    if dictionary:
-        return jsonify(dictionary)
-    else:
-        return jsonify({"error": "Error filtering data"})
-
-
-@app.route("/<class_name>/<id>", methods=["GET"])
-def search_object(class_name, id):
-    obj = DBOperations().search(class_name, id)
-    if obj:
-        return jsonify({"message": f"{class_name} object with ID {id} found"}), 200
-    else:
-        return jsonify({"error": f"No {class_name} object found with ID {id}"}), 404
-
-
-
-# Run the Flask app
 if __name__ == "__main__":
     app.run(debug=True)
