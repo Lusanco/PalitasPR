424b91f1e0a8cfd8d94d6cce51bcaad02e9369c5
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-05 13:25:53 -0400

    Update - Implemented route
    Implemented NotFound view route and button link

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -9,6 +9,7 @@
   import Signup from "./components/Signup.svelte";
   import SignSuccess from "./components/SignSuccess.svelte";
   import ContactUs from "./components/ContactUs.svelte";
+  import NotFound from "./components/NotFound.svelte";
 
   // Import Test Component
   import Pagination from "./components/Pagination.svelte";
@@ -57,6 +58,7 @@
       <Route path="/signup" component={Signup} />
       <Route path="/success" component={SignSuccess} />
       <Route path="/contact" component={ContactUs} />
+      <Route path="/404" component={NotFound} />
 
       <!-- Route  Test Components -->
       <!-- <Route path="/components" component={Pagination} /> -->
