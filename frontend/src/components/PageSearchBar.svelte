a970d691abcb6a6b546e699f67369d8efd6190ee
lusanco <lasc1026@gmail.com>
2024-05-05 10:47:10 -0400

    wip - searchbar updating

--- frontend/src/components/PageSearchBar.svelte
+++ frontend/src/components/PageSearchBar.svelte
@@ -8,7 +8,7 @@
   let hidden = true;
   let loaded = false;
   let search; // service
-  let model = "promotions";
+  let model = "requests";
   let town = "all";
 
   async function handleExplore() {
@@ -36,65 +36,63 @@
 <!-- PageSearchBar Start -->
 <div class="flex flex-col items-center justify-center h-full gap-8">
   <LogoSlogan></LogoSlogan>
-  <div class="items-center justify-center w-full max-w-md">
-    <!-- mobile:1col, FIX WITH GRID SYSTEM 2 ROWS 2 COLS test-commit -->
-    <div id="grid"></div>
-    <!-- SearchBar Start -->
-    <div class="relative bg-white rounded-lg">
-      <label for="Search" class="sr-only"> Search </label>
-      <input
-        type="text"
-        id="search"
-        bind:value={search}
-        placeholder="Search for..."
-        class="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
-      />
-      <!-- Filter Start -->
-      <select
-        id="town-filter"
-        class="block w-full overflow-y-auto border-0 border-b-2 border-gray-200 md:w-24 focus:border-teal-500 focus:ring-0 disabled:cursor-not-allowed"
-      >
-        <option value="">Filter</option>
-        <option value="">Option01</option>
-        <option value="">Option02</option>
-        <option value="">Option03</option>
-      </select>
-      <!-- Filter End -->
-
-      <!-- Filter Start -->
-      <select
-        id="town-filter"
-        class="block w-full overflow-y-auto border-0 border-b-2 border-gray-200 md:w-24 focus:border-teal-500 focus:ring-0 disabled:cursor-not-allowed"
-      >
-        <option value="">Filter</option>
-        <option value="">Option01</option>
-        <option value="">Option02</option>
-        <option value="">Option03</option>
-      </select>
-      <!-- Filter End -->
-
-      <button
-        on:click={handleExplore}
-        type="button"
-        class="w-full h-10 text-teal-600 md:w-20 hover:text-teal-700"
-      >
-        <span class="sr-only">Search</span>
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-5 h-5 m-auto"
+  <!-- SearchBar Start -->
+  <div class="items-center justify-center w-full max-w-md mx-2 md:mx-auto">
+    <div class="grid grid-cols-2 grid-rows-2 bg-white rounded-lg overflow-clip">
+      <div id="search-bar" class="col-span-2 row-span-1">
+        <label for="Search" class="sr-only"> Search </label>
+        <input
+          type="text"
+          id="search"
+          bind:value={search}
+          placeholder="Search for..."
+          class="w-full col-span-2 rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
+        />
+      </div>
+      <div id="filters" class="grid grid-cols-3 col-span-2 row-span-1">
+        <!-- Model Filter Start -->
+        <select
+          bind:value={model}
+          class="block w-full overflow-y-auto border-0 border-b-2 border-gray-200 focus:border-teal-500 focus:ring-0 disabled:cursor-not-allowed"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-          />
-        </svg>
-      </button>
+          <option value="requests">Requests</option>
+          <option value="promotions">Promotions</option>
+        </select>
+        <!-- Model Filter End -->
+        <!-- Town Filter Start -->
+        <select
+          bind:value={town}
+          class="block w-full overflow-y-auto border-0 border-b-2 border-gray-200 focus:border-teal-500 focus:ring-0 disabled:cursor-not-allowed"
+        >
+          <option value="all" disabled>Town</option>
+          <option value="ponce">Ponce</option>
+          <option value="aguadilla">Aguadilla</option>
+          <option value="san-juan">San Juan</option>
+        </select>
+        <!-- Town Filter End -->
+        <button
+          on:click={handleExplore}
+          type="button"
+          class="w-full h-full font-bold text-teal-100 bg-teal-800 hover:bg-teal-700 hover:text-teal-300"
+        >
+          <span class="sr-only">Search</span>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-5 h-5 m-auto"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
     <!-- SearchBar End -->
   </div>
