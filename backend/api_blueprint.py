ddc1a7d338274dbd3ec00b398a6402b4a7094452
Ltoro9 <louistororosario@gmail.com>
2024-05-05 10:45:15 -0400

    route for promotion and sign up

--- backend/api_blueprint.py
+++ backend/api_blueprint.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request, render_template
 from db_operations import DBOperations
 from emails import confirm_email
 import emails
+import uuid
 
 api_bp = Blueprint('api', __name__)
 
@@ -55,7 +56,7 @@ def create_object():
         return {"response": "success"}
     else:
         pass
-    return
+    return jsonify({"error": "Error creating a new object"})
 
 
 @api_bp.route("/filter", methods=["POST"])
@@ -86,10 +87,47 @@ def login():
     return response_html
 
 
-@api_bp.route("/<class_name>/<id>", methods=["GET"])
-def search_object(class_name, id):
-    obj = DBOperations().search(class_name, id)
-    if obj:
-        return jsonify({"message": f"{class_name} object with ID {id} found"}), 200
+# @api_bp.route("/<class_name>/<id>", methods=["GET"])
+# def search_object(class_name, id):
+
+#     obj = DBOperations().search(class_name, id)
+#     if obj:
+#         # Convert the object to a dictionary
+#         obj_dict = obj.__dict__
+#         # Remove the '_sa_instance_state' key (SQLAlchemy internal state)
+#         obj_dict.pop('_sa_instance_state', None)
+#         return jsonify(obj_dict), 200
+#     else:
+#         return jsonify({"error": f"No {class_name} object found with ID {id}"}), 404
+
+
+
+#--------------------------------------------------------------------------
+
+
+@api_bp.route("/signup", methods=["POST"])
+def sign_up():
+    form_data = request.get_json()
+
+    if (
+        "first_name" in form_data
+        and "last_name" in form_data
+        and "email" in form_data
+        and "password" in form_data
+    ):
+        new_obj = DBOperations().sign_up(form_data)
+
+    if new_obj:
+        return {"response": "success"}
     else:
-        return jsonify({"error": f"No {class_name} object found with ID {id}"}), 404
+        pass
+    return jsonify({"error": "Error creating a new object"})
+
+@api_bp.route("/Promotion/<id>", methods=["GET"])
+def show_promo(id):
+    promo_obj = DBOperations().search('Promotion',id)
+    if promo_obj:
+        # obj_dict = promo_obj.all_columns()
+        return jsonify(promo_obj), 200
+    else:
+        return jsonify({"error": f"No Promotion object found with ID {id}"}), 404
