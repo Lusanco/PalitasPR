7d869d9114e9ece3a1d66c1006ef42d68c950344
lusanco <lasc1026@gmail.com>
2024-05-05 20:10:04 -0400

    branch update - awaiting full test

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-  import axios from "axios";
   import { Router, Route } from "svelte-routing";
   import Header from "./components/Header.svelte";
   import Main from "./components/Main.svelte";
@@ -12,44 +11,6 @@
   import AboutUs from "./components/AboutUs.svelte";
   import Faq from "./components/FAQ.svelte";
   import NotFound from "./components/NotFound.svelte";
-
-  // Import Test Component
-  import Pagination from "./components/Pagination.svelte";
-  import UploadImage from "./components/UploadImage.svelte";
-
-  let searchInput = document.getElementById("search-input");
-  let townInput = "All"; // No default town selected
-  let services = []; // Array to store fetched services
-  let errorMessage = ""; // Added to store error messages
-  let switcher = false;
-
-  async function handleSearch() {
-    const data = {
-      Service: {
-        name: searchInput,
-        town: townInput,
-      },
-    };
-
-    axios
-      .post("/api/filter", data)
-      // .get("https://jsonplaceholder.typicode.com/users")
-      .then((response) => {
-        services = response.data;
-        // console.log(services);
-
-        // Loop through each service in the response
-        for (const serviceId in services) {
-          const service = services[serviceId];
-
-          switcher = true;
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        errorMessage = error;
-      });
-  }
 </script>
 
 <div class="flex flex-col">
@@ -64,10 +25,6 @@
       <Route path="/about" component={AboutUs} />
       <Route path="/faq" component={Faq} />
       <Route path="/404" component={NotFound} />
-
-      <!-- Route  Test Components -->
-      <!-- <Route path="/components" component={Pagination} /> -->
-      <!-- <Route path="/components" component={UploadImage} /> -->
     </Router>
   </Main>
   <Footer></Footer>
