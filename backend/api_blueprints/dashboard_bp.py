37e6d8ce561819e1ec74f20097dc47ca3d03daeb
lusanco <lasc1026@gmail.com>
2024-05-24 16:18:35 -0400

    changed to data

--- backend/api_blueprints/dashboard_bp.py
+++ backend/api_blueprints/dashboard_bp.py
@@ -1,39 +1,51 @@
 from flask import Blueprint, jsonify, request, make_response, session
 from db_operations import DBOperations
 import emails
-from flask_login import login_user, logout_user, login_required, current_user,LoginManager
+from flask_login import (
+    login_user,
+    logout_user,
+    login_required,
+    current_user,
+    LoginManager,
+)
 from werkzeug.utils import secure_filename
 import aws_bucket
-my_bp = Blueprint('my', __name__)
+
+my_bp = Blueprint("my", __name__)
+
 
 @my_bp.before_request
 def keep_session_alive():
-    session.modified = True # Before requests, keep alive session if it hasnt expired
+    session.modified = True  # Before requests, keep alive session if it hasnt expired
 
-@my_bp.route('/promotion-request', methods=['GET', 'POST', 'PUT'])
+
+@my_bp.route("/promotion-request", methods=["GET", "POST", "PUT"])
 @login_required
 def promo_request():
-    '''
-        Route to get all promo and request posted by a user
-    '''
+    """
+    Route to get all promo and request posted by a user
+    """
 
     # GET METHOD:
-    if request.method == 'GET':
+    if request.method == "GET":
 
         # Retrieve all promotions and requests from a user
         results = DBOperations().promo_request(current_user.id)
-        return(make_response({'results': results}), 200) # 2 dicts, (<{promos}>, <{requests}>)
+        return (
+            make_response({"results": results}),
+            200,
+        )  # 2 dicts, (<{promos}>, <{requests}>)
 
     # PUT METHOD:
-    if request.method == 'PUT':
+    if request.method == "PUT":
         frontend_data = {
-            'model': 'Promotion',
-            'id': '7533c8eb-6b8a-4bf5-a301-a064d1bc21c3',
-            'title': 'Modified Title',
-            'price_min': 30
-            }
-        model = frontend_data.get('model')
-        frontend_data.pop('model')
+            "model": "Promotion",
+            "id": "7533c8eb-6b8a-4bf5-a301-a064d1bc21c3",
+            "title": "Modified Title",
+            "price_min": 30,
+        }
+        model = frontend_data.get("model")
+        frontend_data.pop("model")
         response, status = DBOperations().update({model: frontend_data})
         if status == 200:
             return make_response(jsonify(response), 200)
@@ -41,7 +53,7 @@ def promo_request():
             return make_response(jsonify(response), status)
 
     # POST METHOD:
-    if request.method == 'POST':
+    if request.method == "POST":
 
         # frontend_data= {
         # 'user_id': current_user.id,
@@ -54,28 +66,31 @@ def promo_request():
         # 'town': [1, 2]
         # }
         data = request.get_json()
-        model = data.get('model')
-        town_id = frontend_data.get('town')
-        frontend_data.pop('town')
-        frontend_data.pop('model')
+        print(f"Data from front: {data}")
+        data["user_id"] = current_user.id
+        model = data.get("model")
+        town_id = data.get("town")
+        data.pop("town")
+        data.pop("model")
 
         # Create (Promo or Request)
-        response, status = DBOperations().new({model:frontend_data})
-        
-        if status == 201: # Ok status
-            objectDict = response
-            
-            response, status = DBOperations().new({'Promo_Towns':{'promo_id': objectDict['id'], 'town_id': town_id }})
+        response, status = DBOperations().new({model: data})
 
-            # Check status code, exit loop of towns 
+        if status == 201:  # Ok status
+            objectDict = response
+
+            response, status = DBOperations().new(
+                {"Promo_Towns": {"promo_id": objectDict["id"], "town_id": town_id}}
+            )
+
+            # Check status code, exit loop of towns
             if status != 201:
-                return make_response(jsonify({'error': 'Adding town error'}), 500)
+                return make_response(jsonify({"error": "Adding town error"}), 500)
 
             # Success
             return make_response(jsonify(response), 201)
         else:
-            return make_response(jsonify({'error': response}), status)
-
+            return make_response(jsonify({"error": response}), status)
 
     # POST Method
     # if request.method == 'POST':
