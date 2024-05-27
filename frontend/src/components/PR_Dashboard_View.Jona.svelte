f6b722344a1c13805eaa7c7db1607bd6148613e7
lusanco <lasc1026@gmail.com>
2024-05-27 14:58:53 -0400

    Pulling Jona's components from feature/components

---
+++ frontend/src/components/PR_Dashboard_View.Jona.svelte
@@ -0,0 +1,144 @@
+<!-- ? Promotion/Request after Search View -->
+<!-- 
+  TODO: 
+    * Implement data from backend
+    * Implement functions with real logic to handle the buttons
+    * Implement Card(Component) close logic
+-->
+
+<script>
+  import OffersCards from "./OffersCards.svelte";
+
+  /**
+   * ! Test data
+   */
+
+  let offers = [
+    {
+      name: "Jane Doe",
+      id: Math.floor(Math.random() * 1000000),
+    },
+    {
+      name: "John Doe",
+      id: Math.floor(Math.random() * 1000000),
+    },
+    {
+      name: "John Smith",
+      id: Math.floor(Math.random() * 1000000),
+    },
+    {
+      name: "Jane Smith",
+      id: Math.floor(Math.random() * 1000000),
+    },
+    {
+      name: "John Doe",
+      id: Math.floor(Math.random() * 1000000),
+    },
+    {
+      name: "John Smith",
+      id: Math.floor(Math.random() * 1000000),
+    },
+    {
+      name: "Jane Smith",
+      id: Math.floor(Math.random() * 1000000),
+    },
+  ];
+
+  /**
+   * ? User data
+   */
+
+  let image = {
+    img: "https://images.pexels.com/photos/1453499/pexels-photo-1453499.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "Lawn Mowing",
+  };
+
+  let service = "Lawn Mowing";
+
+  let description = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium,
+          magni nisi? Repellendus sequi perspiciatis delectus esse excepturi
+          laborum, labore debitis quos sunt doloribus nesciunt quia quibusdam
+          sed. Culpa, corrupti nisi.`;
+
+  /**
+   * ! Test functions
+   */
+
+  function handleCard() {
+    alert("Close button is working!");
+  }
+
+  function viewOffer() {
+    alert("View Review button is working!");
+  }
+</script>
+
+<!-- 
+  ? Container
+-->
+<div
+  class="flex flex-col items-center justify-center min-h-screen p-5 bg-slate-100"
+>
+  <!-- 
+    * Card 
+  -->
+  <div
+    class="relative w-full max-w-6xl p-5 py-4 mx-2 rounded-md shadow-md bg-slate-100"
+  >
+    <!-- 
+      * Exit button 
+    -->
+    <button
+      on:click={handleCard}
+      class="absolute justify-end w-8 top-4 right-2"
+    >
+      <i class="fa-solid fa-xmark"></i>
+    </button>
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+      <!-- 
+        * Left Side Section 
+      -->
+      <div class="col-span-1 p-2 m-1">
+        <h1 class="text-3xl text-center">Request</h1>
+        <figure class="mb-2 border-b-2">
+          <img
+            src={image.img}
+            alt={image.alt}
+            class="object-cover w-full h-64 mt-3 rounded-md"
+          />
+          <div class="mt-2">
+            <h2 class="text-lg">John Smith</h2>
+            <p class="mb-1 text-sm">Service requested: {service}</p>
+          </div>
+        </figure>
+        <p class="mt-2 text-sm lg:text-base">{description}</p>
+      </div>
+      <!-- 
+        * Right Side Section 
+      -->
+      <div class="col-span-1 p-2 m-1">
+        <div class="grid grid-cols-3">
+          <h1
+            class="col-span-3 text-3xl text-center lg:col-span-1 lg:col-start-2"
+          >
+            Offers
+          </h1>
+        </div>
+
+        <div class="flex flex-col mt-2">
+          <div class="flex flex-col gap-1 overflow-y-scroll element h-[26rem]">
+            {#each offers as offer (offer.id)}
+              <!-- 
+                * Offers Cards Component 
+              -->
+              <OffersCards name={offer.name} id={offer.id} {viewOffer} />
+            {/each}
+          </div>
+        </div>
+      </div>
+      <!-- 
+        * Action Buttons 
+      -->
+    </div>
+  </div>
+</div>
---
+++ frontend/src/components/ReviewView.Jona.svelte
@@ -0,0 +1,100 @@
+<!-- ? Promotion/Request after Search View -->
+<!-- 
+  TODO:
+
+    * Implement data from backend
+    * Implement functions with real logic to handle the buttons
+    * Implement Card(Component) close logic
+-->
+
+<script>
+  /**
+   * ? User data
+   */
+
+  let user = {
+    name: "Jane Doe",
+    id: Math.floor(Math.random() * 1000000),
+    rating: Math.floor(Math.random() * 5) + 1,
+  };
+
+  let service = {
+    name: "Lawn Mowing",
+    id: Math.floor(Math.random() * 1000000),
+    rating: Math.floor(Math.random() * 5) + 1,
+  };
+
+  let review = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium,
+          magni nisi? Repellendus sequi perspiciatis delectus esse excepturi
+          laborum, labore debitis quos sunt doloribus nesciunt quia quibusdam
+          sed. Culpa, corrupti nisi. Quisquam, quod. Quisquam, quod. Quisquam,
+          quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quis
+          fuga totam quia hic, aperiam ratione voluptates necessitatibus error iure
+          sint mollitia quaerat quae excepturi obcaecati praesentium facilis sapiente
+          minus!`;
+
+  /**
+   * ! Test functions
+   */
+</script>
+
+<!-- 
+  ? Container
+-->
+<div
+  class="flex flex-col items-center justify-center min-h-screen p-6 md:p-12 bg-slate-100"
+>
+  <!-- 
+    * Card 
+  -->
+  <div class="w-full p-5 rounded shadow-md bg-slate-100">
+    <div class="flex justify-between mx-1">
+      <!--
+        * User and Service Details
+      -->
+      <div>
+        <h2 class="mt-1 text-lg">{user.name}</h2>
+        <h3 class="mb-1 -mt-1 text-sm">ID: {user.id}</h3>
+      </div>
+      <div>
+        <h2 class="mt-1 text-lg">{service.name}</h2>
+        <h3 class="mb-1 -mt-1 text-sm text-end">ID: {service.id}</h3>
+      </div>
+    </div>
+    <!--
+      * Review Box
+    -->
+    <div class="p-4 my-2 rounded bg-slate-200">
+      <p class="text-wrap">
+        {review}
+      </p>
+    </div>
+    <!--
+      * User and Service Ratings
+    -->
+    <div class="flex justify-between mx-1">
+      <div class="mt-2">
+        <h2>
+          User rating: <i class="text-sm text-yellow-400 fa-solid fa-star"></i>
+          {user.rating}
+        </h2>
+        <h2 class="-mt-1">
+          Job Rating: <i class="text-sm text-yellow-400 fa-solid fa-star"></i>
+          {service.rating}
+        </h2>
+      </div>
+      <!--
+        * Action Button
+      -->
+      <div>
+        <button
+          class="flex p-2 px-6 mt-4 text-white bg-teal-500 rounded-md hover:bg-teal-600"
+          ><i class="block p-1 fa-solid fa-xmark md:hidden"></i>
+          <span class="hidden md:block">Close</span></button
+        >
+      </div>
+    </div>
+  </div>
+</div>
+
+<style></style>
