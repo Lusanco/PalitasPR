f0b929501c8d3fbe83559a2c777649b46b132c81
lusanco <lasc1026@gmail.com>
2024-05-28 16:13:48 -0400

    added team and members relatend component for About page

--- frontend/src/components/Member.svelte
+++ frontend/src/components/Member.svelte
@@ -13,15 +13,3 @@
     <p class={classLIST.classP2}>{memberROLE}</p>
   </div>
 </div>
-
-<!-- <div class="flex flex-col items-center justify-center">
-  <img
-    class="object-cover w-20 h-20 mb-2 rounded-full shadow"
-    src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-    alt="Person"
-  />
-  <div class="flex flex-col items-center">
-    <p class="text-lg font-bold">Luis Santiago</p>
-    <p class="text-sm text-gray-800">Team Lead</p>
-  </div>
-</div> -->
