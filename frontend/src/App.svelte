eda188ae4b68158c10100648e61c06d9803aa14c
Lusanco <lasc1026@gmail.com>
2024-06-03 14:38:02 -0400

    manage requests and services

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -18,8 +18,8 @@
   import CreateServiceSuccess from "./routes/CreateServiceSuccess.svelte";
   import CreateRequest from "./routes/CreateRequest.svelte";
   import CreateRequestSuccess from "./routes/CreateRequestSuccess.svelte";
-  import Offers from "./routes/Offers.svelte";
-  import Services from "./routes/Services.svelte";
+  import ManageServices from "./routes/ManageServices.svelte";
+  import ManageRequests from "./routes/ManageRequests.svelte";
   import Profile from "./routes/Profile.svelte";
   import About from "./routes/About.svelte";
   import Contact from "./routes/Contact.svelte";
@@ -40,15 +40,12 @@
       <Route path="/create-service-success" component={CreateServiceSuccess} />
       <Route path="/create-request" component={CreateRequest} />
       <Route path="/create-request-success" component={CreateRequestSuccess} />
-      <Route path="/offers" component={Offers} />
-      <Route path="/services" component={Services} />
+      <Route path="/manage-services" component={ManageServices} />
+      <Route path="/manage-requests" component={ManageRequests} />
       <Route path="/profile" component={Profile} />
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
       <Route path="/faq" component={Faq} />
-
-      <!-- Testing Layout/Route/Component Currently Work On -->
-      <!-- <Route path="/test" component={ReviewView} /> -->
     </Router>
   </Main>
   <Footer />
--- frontend/src/routes/Dashboard.svelte
+++ frontend/src/routes/Dashboard.svelte
@@ -1,28 +1,21 @@
 <script>
   import Link from "../components/Link.svelte";
 
-  /**
-   *? Button Styling
-   */
-
   let twcss =
     "w-full h-24 md:h-32 rounded-lg bg-teal-600 flex flex-col justify-center items-center font-bold shadow-md text-white text-md md:text-2xl hover:bg-teal-700 transition duration-300 ease-in-out cursor-pointer text-center";
 </script>
 
 <div class="flex flex-col items-center justify-center h-full min-h-screen">
-  <!-- 
-  * Grid
--->
   <div class="flex w-full">
     <div
       class="grid w-full grid-cols-2 grid-rows-2 gap-2 p-4 m-4 md:p-8 md:gap-3 md:m-12"
     >
       <Link name="Create Service" {twcss} href="/create-service"></Link>
       <Link name="Create Request" {twcss} href="/create-request"></Link>
-      <Link name="Offers" {twcss} href="/offers"></Link>
-      <Link name="Services" {twcss} href="/services"></Link>
+      <Link name="Manage Services" {twcss} href="/manage-services"></Link>
+      <Link name="Manage Requests" {twcss} href="/manage-requests"></Link>
+      <Link name="Inbox" {twcss} href="/inbox"></Link>
       <Link name="Profile" {twcss} href="/profile"></Link>
-      <Link name="Settings" {twcss} href="/settings"></Link>
     </div>
   </div>
 </div>
