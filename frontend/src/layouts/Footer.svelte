d5a15bc127e2e512b70d21f0b278fe03f2312bf5
Lusanco <lasc1026@gmail.com>
2024-06-08 04:23:24 -0400

    updating links

--- frontend/src/layouts/Footer.svelte
+++ frontend/src/layouts/Footer.svelte
@@ -1,12 +1,16 @@
+<script>
+  import { link } from "svelte-routing";
+</script>
+
 <footer
   class="flex flex-wrap justify-between p-10 rounded footer bg-base-100 text-base-content"
 >
   <nav class="flex flex-wrap items-center justify-center gap-4 mx-auto">
-    <a href="/about" class="link link-hover">About us</a>
-    <a href="/contact" class="link link-hover">Contact</a>
-    <a href="/faq" class="link link-hover">FAQ</a>
-    <a href="/terms-of-use" class="link link-hover">Terms of Use</a>
-    <a href="/privacy" class="link link-hover">Privacy</a>
+    <a use:link href="/terms-of-use" class="link link-hover">Terms of Use</a>
+    <a use:link href="/privacy" class="link link-hover">Privacy</a>
+    <a use:link href="/about" class="link link-hover">About us</a>
+    <a use:link href="/contact" class="link link-hover">Contact</a>
+    <a use:link href="/faq" class="link link-hover">FAQ</a>
   </nav>
   <p class="mx-auto text-center md:text-end">
     Copyright © 2024 - All right reserved by PalitasPR
--- frontend/src/layouts/Header.svelte
+++ frontend/src/layouts/Header.svelte
@@ -1,15 +1,16 @@
 <!-- Header Layout -->
 <script>
+  import { link } from "svelte-routing";
 </script>
 
 <!-- Header Start -->
 <header class="navbar bg-base-100">
   <div class="navbar-start">
-    <a href="/" class="text-xl btn btn-ghost">PalitasPR</a>
+    <a use:link href="/" class="text-xl btn btn-ghost">PalitasPR</a>
   </div>
   <div class="gap-2 navbar-end">
-    <a href="/login" class="btn">Login</a>
-    <a href="/signup" class="btn">Signup</a>
+    <a use:link href="/login" class="btn">Login</a>
+    <a use:link href="/signup" class="btn">Signup</a>
   </div>
 </header>
 <!-- Header End -->
