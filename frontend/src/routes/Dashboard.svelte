354e242a11676adabced504f7d0d3917f3b50036
Lusanco <lasc1026@gmail.com>
2024-06-13 12:07:51 -0400

    update dashboard

--- frontend/src/routes/Dashboard.svelte
+++ frontend/src/routes/Dashboard.svelte
@@ -2,7 +2,7 @@
   import Link from "../components/Link.svelte";
 
   let twcss =
-    "w-full h-24 md:h-32 rounded-lg bg-teal-600 flex flex-col justify-center items-center font-bold shadow-md text-white text-md md:text-2xl hover:bg-teal-700 transition duration-300 ease-in-out cursor-pointer text-center";
+    "w-full h-24 md:h-32 rounded-lg bg-stone-200  flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-teal-700 transition duration-300 ease-in-out cursor-pointer text-center";
 </script>
 
 <div class="flex flex-col items-center justify-center h-full min-h-screen">
@@ -14,7 +14,7 @@
       <Link name="Create Request" {twcss} href="/create-request"></Link>
       <Link name="Manage Services" {twcss} href="/manage-services"></Link>
       <Link name="Manage Requests" {twcss} href="/manage-requests"></Link>
-      <Link name="Inbox" {twcss} href="/inbox"></Link>
+      <Link name="Tasks" {twcss} href="/inbox"></Link>
       <Link name="Profile" {twcss} href="/profile"></Link>
     </div>
   </div>
