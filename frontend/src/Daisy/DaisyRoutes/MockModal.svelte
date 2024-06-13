290341688df1e9234004ab7a159987f6616a2d37
Livanhernandez <livanhernandez9@gmail.com>
2024-06-13 15:11:57 -0400

    Add - Modal

---
+++ frontend/src/Daisy/DaisyRoutes/MockModal.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let showModal = false;
+  export let onClose;
+
+  function closeModal() {
+    onClose();
+  }
+</script>
+
+{#if showModal}
+  <div class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+    <div class="w-10/12 p-6 bg-white rounded-lg shadow-lg md:w-1/2">
+      <h2 class="mb-4 text-xl font-bold">Autorización de Contacto</h2>
+      <p class="mb-4">Al aceptar, usted autoriza proporcionar sus datos de contacto y ser contactado por el dueño del post.</p>
+      <div class="flex justify-end">
+        <button class="mr-2 btn bg-stone-200" on:click={closeModal}>Cancelar</button>
+        <button class="mr-2 btn bg-stone-200" on:click={closeModal}>Aceptar</button>
+      </div>
+    </div>
+  </div>
+{/if}
