4f826091bb58a0fc19f99593d13af5f6385709b1
Livanhernandez <livanhernandez9@gmail.com>
2024-06-14 20:44:27 -0400

    Add css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,11 +4,24 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require('daisyui'),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("daisyui")],
   daisyui: {
-    themes: false,
+    themes: [
+      {
+        mytheme: {
+          primary: "#0F766E", // bg-teal-800
+          "primary-content": "#E6FFFA", // text-teal-100
+          secondary: "#2DD4BF", // bg-teal-400
+          "secondary-content": "#0F766E", // text-teal-800
+          accent: "#5EEAD4", // bg-teal-400
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
   },
 };
