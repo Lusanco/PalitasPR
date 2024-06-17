f71b5d14bead11c76e9f236f22a6899e7715c285
Lusanco <lasc1026@gmail.com>
2024-06-17 15:14:40 -0400

    update - app.py for static folder serve, weird interaction in change from one url from another

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """MAIN APP WITH FLASK"""
 
-from flask import Flask, session, g
+from flask import Flask, send_from_directory, session, g
 from flask_cors import CORS
 from db.db_operations import DBOperations
 from api_blueprints.api_blueprint import api_bp
@@ -14,24 +14,32 @@ from flask_login import LoginManager
 from datetime import timedelta
 from db_init import init_db
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path="/")
+
+
+@app.route("/")
+def serve_index():
+    return send_from_directory(app.static_folder, "index.html")
+
+
 app.config["SECRET_KEY"] = "demo_dev_pwd"
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = "antoniofdjs@gmail.com"
 app.config["MAIL_PASSWORD"] = "syhk sijd eoli tgba"
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=15)
-app.register_blueprint(api_bp, url_prefix='/api')
-app.register_blueprint(my_bp, url_prefix='/api/dashboard')
-app.register_blueprint(user_bp, url_prefix='/api/user')
-app.register_blueprint(promotion_bp, url_prefix='/api/promotion')
-app.register_blueprint(task_bp, url_prefix='/api/tasks')
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=15)
+app.register_blueprint(api_bp, url_prefix="/api")
+app.register_blueprint(my_bp, url_prefix="/api/dashboard")
+app.register_blueprint(user_bp, url_prefix="/api/user")
+app.register_blueprint(promotion_bp, url_prefix="/api/promotion")
+app.register_blueprint(task_bp, url_prefix="/api/tasks")
 
 mail = Mail(app)
 login_manager = LoginManager(app)
 
 CORS(app)
+
 
 @app.before_request
 def keep_session_alive():
@@ -42,14 +50,14 @@ def keep_session_alive():
 def load_user(user_id):
     # Assuming `user_id` is the primary key of the user in your database
     # Implement your logic to load a user from the database based on `user_id`
-    user = DBOperations().search('User', user_id)
+    user = DBOperations().search("User", user_id)
     return user
 
 
 # This function will be called after each request to close the session
 @app.teardown_request
 def close_session(exception=None):
-    session = g.pop('session', None)
+    session = g.pop("session", None)
     if session is not None:
         session.close()
 
