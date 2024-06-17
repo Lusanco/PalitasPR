acb3a592bafb1fb615cedf4904be43260f48fd0c
Livanhernandez <livanhernandez9@gmail.com>
2024-06-17 19:37:30 -0400

    Add - Added file with manage service and manage request button removed.

---
+++ frontend/src/MVP/MVP_Dashboard.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Link from "../components/Link.svelte";
+
+  let twcss =
+    "w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-[#f1f1f1] flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center";
+</script>
+
+<head>
+  <title>PalitasPR | Dashboard</title>
+</head>
+
+<div class="flex flex-col items-center justify-center h-full min-h-screen bg-white">
+  <div class="flex w-full">
+    <div class="grid w-full grid-cols-2 grid-rows-2 gap-2 p-4 m-4 md:p-8 md:gap-3 md:m-12">
+      <Link name="Create Service" {twcss} href="/create-service"></Link>
+      <Link name="Create Request" {twcss} href="/create-request"></Link>
+      <Link name="Tasks" {twcss} href="/inbox"></Link>
+      <Link name="Profile" {twcss} href="/profile"></Link>
+    </div>
+  </div>
+</div>
