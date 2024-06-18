cce1ff32aaba81609f4a2df50fde4e2b83d2664a
prodjohnper <JonathanPerez9743@gmail.com>
2024-06-18 00:08:46 -0400

    Add - Updated component with new color scheme

---
+++ frontend/src/MVP/MVP_Main.svelte
@@ -0,0 +1,43 @@
+<!-- Main Layout -->
+<script>
+  import Menu from "../components/Menu.svelte";
+  import Button from "../components/Button.svelte";
+
+  // Button Prop Variables And Dependencies
+  let image = null;
+  let button = {
+    name: "",
+    method: "",
+    url: "",
+    headers: "",
+    twcss:
+      "flex flex-col absolute items-center justify-center w-16 h-16 p-2 text-sm text-[#1f1f1f] rounded-lg hover:bg-[#cc2936] hover:text-[#f1f1f1] focus:outline-none focus:ring-0 m-2 z-50",
+    misc: { "App Location": "Back Button Component" },
+  };
+  // Button Prop Variables And Dependencies
+</script>
+
+<!-- Main Start -->
+<main class="min-h-full overflow-y-scroll element">
+  <Button {image} {button}>
+    <span class="sr-only">Go back</span>
+    <svg
+      class="w-5 h-5"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M15 19l-7-7 7-7"
+      />
+    </svg>
+  </Button>
+  <!-- <Menu></Menu> -->
+  <slot />
+</main>
+<!-- Main End -->
