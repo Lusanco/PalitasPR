97b4f970654d6c80af7a9ee6544cc945c68821b6
prodjohnper <JonathanPerez9743@gmail.com>
2024-06-20 00:01:00 -0400

    Update - Updated email.py to implement email template for email confirmation interaction

--- backend/emails.py
+++ backend/emails.py
@@ -14,33 +14,49 @@ import bcrypt
 import aws_bucket
 
 
-engine = create_engine('postgresql://demo_dev:demo_dev_pwd@demodb.ctossyay6vcz.us-east-2.rds.amazonaws.com/postgres')    
+engine = create_engine(
+    'postgresql://demo_dev:demo_dev_pwd@demodb.ctossyay6vcz.us-east-2.rds.amazonaws.com/postgres')
+
+
+def load_html_template(template_path):
+    with open(template_path, 'r') as file:
+        return file.read()
+
 
 def send_confirm_email(email, first_name, token):
-        '''
-            Receives email, first name and token to send
-            in the body of the email.
-            Person receives a link to verify the email.
-        '''
-        from app import mail, app
-        from flask_mail import Message
-        from flask import url_for
+    '''
+        Receives email, first name and token to send
+        in the body of the email.
+        Person receives a link to verify the email.
+    '''
+    import os
+    from flask_mail import Message
+    from app import mail, app
 
-        subject = "Welcome to PalitasPR"
-        confirm_link = f"http://127.0.0.1:5000/api/user/verify_email/{token}"
+    subject = "Welcome to PalitasPR"
+    confirm_link = f"http://127.0.0.1:5000/api/user/verify_email/{token}"
 
-        body = f"Hello,\n\nThank you for signing up {first_name}. Please click: {confirm_link} to verify your email."
+    # Load the HTML template
+    html_template = load_html_template(
+        '../email/build_production/transactional.html')
 
-        msg = Message(subject, sender=app.config['MAIL_USERNAME'], recipients=[email])
-        msg.body = body
-        print('TRYING EMAIL')
-        try:
-            mail.send(msg)
-            print('Email sent')
-            return "Email Sent Successfully"  # Add a return message for the user
-        except Exception as e:
-            print(f"Error sending email: {e}")
-            return f"Error Sending Email: {e}"  # Inform user about the error
+    # Inject dynamic content into the HTML template
+    html_content = html_template.replace(
+        '{{first_name}}', first_name).replace('{{confirm_link}}', confirm_link)
+
+    msg = Message(
+        subject, sender=app.config['MAIL_USERNAME'], recipients=[email])
+    msg.html = html_content  # Set the HTML content of the email
+
+    print('TRYING EMAIL')
+    try:
+        mail.send(msg)
+        print('Email sent')
+        return "Email Sent Successfully"  # Add a return message for the user
+    except Exception as e:
+        print(f"Error sending email: {e}")
+        return f"Error Sending Email: {e}"  # Inform user about the error
+
 
 def confirm_email(token):
     '''
@@ -51,7 +67,7 @@ def confirm_email(token):
     Session = sessionmaker(bind=engine)
     session = Session()
     print(token)
-    user = session.query(User).filter_by(verification_token = token).first()
+    user = session.query(User).filter_by(verification_token=token).first()
     if user:
         response, status = aws_bucket.create_user_folder(user.id)
         if status == 201:
@@ -60,7 +76,8 @@ def confirm_email(token):
             user.verified = True
         session.commit()
     else:
-        return ({'error': 'No user with that token'}, 404) # No user with that token
+        # No user with that token
+        return ({'error': 'No user with that token'}, 404)
 
     session.close()
-    return (response, 200) # OK
+    return (response, 200)  # OK
