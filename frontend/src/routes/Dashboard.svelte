7af46612bd6e4d6c80ac093c548796fb82ef6f22
Livanhernandez <livanhernandez9@gmail.com>
2024-06-21 14:36:34 -0400

    Add - Added dashboard with bg color changed to base and button bg color to white with a max-w-6xl on the buttons

--- frontend/src/routes/Dashboard.svelte
+++ frontend/src/routes/Dashboard.svelte
@@ -2,15 +2,15 @@
   import Link from "../components/Link.svelte";
 
   let twcss =
-    "w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-[#f1f1f1] flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center";
+    "w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-white flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center";
 </script>
 
 <head>
   <title>PalitasPR | Dashboard</title>
 </head>
 
-<div class="flex flex-col items-center justify-center h-full min-h-screen bg-white">
-  <div class="flex w-full">
+<div class="flex flex-col items-center justify-center h-full min-h-screen bg-base">
+  <div class="flex w-full max-w-6xl">
     <div class="grid w-full grid-cols-2 grid-rows-2 gap-2 p-4 m-4 md:p-8 md:gap-3 md:m-12">
       <Link name="Create Service" {twcss} href="/create-service"></Link>
       <Link name="Create Request" {twcss} href="/create-request"></Link>
