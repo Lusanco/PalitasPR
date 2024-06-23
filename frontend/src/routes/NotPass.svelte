4483881fea3636611bdbd2f8375fa6b7f19bcea9
Lusanco <lasc1026@gmail.com>
2024-06-23 11:49:23 -0400

    dev - enjoy easier development

---
+++ frontend/src/routes/NotPass.svelte
@@ -0,0 +1,15 @@
+<script>
+  import axios from "axios";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    axios
+      .get("/api/user/logout")
+      .then((loginRes) => {
+        console.log("Logout Success!", loginRes.data);
+      })
+      .catch((loginError) => {
+        console.log("Logout Error", loginError);
+      });
+  });
+</script>
