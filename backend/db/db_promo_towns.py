1f41c686eed5e4537655fd30d1bd9568c5d53e6e
Antonio De Jesus Santiago <142444999+Antoniofdjs@users.noreply.github.com>
2024-06-23 21:55:23 -0400

    CREATED - db_promo_towns to handle db ops for this clas

---
+++ backend/db/db_promo_towns.py
@@ -0,0 +1,40 @@
+'''
+    All related functions for Promo_Towns class that involves the database
+    and routes from flask.
+'''
+from sqlalchemy.orm import joinedload
+from models import User
+from sqlalchemy import func
+from models import (
+    User, Service, Town, Promo_Towns, 
+    Promotion, Review, Task,
+) 
+
+
+class Db_promo_towns:
+    '''
+        Class to call when using any Promotion functionality
+        required.
+
+        Examples:
+        * Promotion queries or mainly related to Promotion
+        * Functionalites to filter certain Promotions
+        * etc..
+    '''
+    def __init__(self, db_session):
+        self.session = db_session
+
+    def get_towns_for_promo(self, promoID):
+        '''
+            Returns List of all town names where promo is available or None 
+        '''
+        town_names = self.session.query(Town.name)\
+            .join(Promo_Towns, Promo_Towns.town_id == Town.id)\
+            .filter(Promo_Towns.promo_id == promoID)\
+            .all()
+        if not town_names:
+            return None
+        list_ofTowns = []
+        for town in town_names:
+            list_ofTowns.append(town[0])
+        return list_ofTowns
