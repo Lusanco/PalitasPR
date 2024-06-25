90d0fe446c4d6b477f155487bc53070116eee88f
Livanhernandez <livanhernandez9@gmail.com>
2024-06-24 22:49:33 -0400

    add - added file user session implemented.

--- frontend/src/routes/InitialContactSuccess.svelte
+++ frontend/src/routes/InitialContactSuccess.svelte
@@ -1,5 +1,23 @@
 <script>
   import { link } from "svelte-routing";
+  import { onMount } from "svelte";
+  import { userSession } from "../scripts/stores";
+  import axios from "axios";
+
+  onMount(() => {
+    axios
+    .get("/api/user/status")
+    .then((userStatusRes) => {
+      userSession.set(true);
+      console.log(userStatusRes.data);
+    })
+    .catch((userStatusErr) => {
+      userSession.set(false);
+      console.log(userStatusErr);
+      console.log($userSession)
+    })
+  })
+  
 </script>
 
 <head>
