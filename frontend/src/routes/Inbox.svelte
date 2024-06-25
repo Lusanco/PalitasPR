f16deb92bf9f524a0ff83f11b3372cef9dc17b54
Livanhernandez <livanhernandez9@gmail.com>
2024-06-24 22:49:25 -0400

    add - added file user session implemented.

--- frontend/src/routes/Inbox.svelte
+++ frontend/src/routes/Inbox.svelte
@@ -1,5 +1,23 @@
 <!-- After click it should redirect to Service where the offer was made/tied to -->
 <script>
+  import { onMount } from "svelte";
+  import { userSession } from "../scripts/stores";
+  import axios from "axios";
+
+  onMount(() => {
+    axios
+    .get("/api/user/status")
+    .then((userStatusRes) => {
+      userSession.set(true);
+      console.log(userStatusRes.data);
+    })
+    .catch((userStatusErr) => {
+      userSession.set(false);
+      console.log(userStatusErr);
+      console.log($userSession);
+    })
+  })
+
   let offers = [
     {
       id: 1,
