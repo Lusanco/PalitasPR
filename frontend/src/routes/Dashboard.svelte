83d8f3083bc23b42bfb404d3e7fc88f54b71cf14
Livanhernandez <livanhernandez9@gmail.com>
2024-06-26 22:16:50 -0400

    added file Dashboard.svelte with Profile linked in dashboard

--- frontend/src/routes/Dashboard.svelte
+++ frontend/src/routes/Dashboard.svelte
@@ -3,6 +3,9 @@
   import { userSession } from "../scripts/stores";
   import axios from "axios";
   import { Link } from "svelte-routing";
+  import { writable } from "svelte/store";
+
+  let profileID = writable()
 
   // Check if the user is logged in
   onMount(() => {
@@ -11,6 +14,7 @@
       .get("/api/user/status")
       .then((userStatusRes) => {
         userSession.set(true);
+        profileID.set(userStatusRes.data.profile_id)
         console.log(userStatusRes.data);
       })
       // Catch errors
@@ -51,7 +55,7 @@
       <!--* Profile button -->
       <Link
         class="w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-white flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center"
-        to="/profile">Profile</Link
+        to={`/profile/${$profileID}`}>Profile</Link
       >
     </div>
   </div>
