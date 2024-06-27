9635673a978fde92d3eb8b5e7603c97db05c0a9d
prodjohnper <JonathanPerez9743@gmail.com>
2024-06-26 20:58:18 -0400

    Update - Implemented documentation and deleted unused tailwind styling variable

--- frontend/src/routes/Dashboard.svelte
+++ frontend/src/routes/Dashboard.svelte
@@ -4,22 +4,22 @@
   import axios from "axios";
   import { Link } from "svelte-routing";
 
+  // Check if the user is logged in
   onMount(() => {
     axios
+      // Make a GET request to the user status endpoint
       .get("/api/user/status")
       .then((userStatusRes) => {
         userSession.set(true);
         console.log(userStatusRes.data);
       })
+      // Catch errors
       .catch((userStatusErr) => {
         userSession.set(false);
         console.log(userStatusErr);
         console.log($userSession);
       });
   });
-
-  let twcss =
-    "w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-white flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center";
 </script>
 
 <head>
@@ -33,18 +33,22 @@
     <div
       class="grid w-full grid-cols-2 grid-rows-2 gap-2 p-4 m-4 md:p-8 md:gap-3 md:m-12"
     >
+      <!--* Create Service button -->
       <Link
         class="w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-white flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center"
         to="/create-service">Create Service</Link
       >
+      <!--* Create Request button -->
       <Link
         class="w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-white flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center"
         to="/create-request">Create Request</Link
       >
+      <!--* Tasks button -->
       <Link
         class="w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-white flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center"
         to="/tasks">Tasks</Link
       >
+      <!--* Profile button -->
       <Link
         class="w-full h-24 md:h-32 rounded-lg text-[#1f1f1f] bg-white flex flex-col justify-center items-center font-bold shadow-md text-md md:text-2xl hover:bg-[#cc2936] hover:text-white transition duration-300 ease-in-out cursor-pointer text-center"
         to="/profile">Profile</Link
