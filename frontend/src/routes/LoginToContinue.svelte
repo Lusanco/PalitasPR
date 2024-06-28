98bf049f14c73a999eb9ed1d14a76b97c825a1e3
prodjohnper <JonathanPerez9743@gmail.com>
2024-06-27 21:12:02 -0400

    Update - tab dynamic title implementation

--- frontend/src/routes/LoginToContinue.svelte
+++ frontend/src/routes/LoginToContinue.svelte
@@ -6,26 +6,22 @@
 
   onMount(() => {
     axios
-    .get("/api/user/status")
-    .then((userStatusRes) => {
-      userSession.set(true);
-      console.log(userStatusRes.data);
-    })
-    .catch((userStatusErr) => {
-      userSession.set(false);
-      console.log(userStatusErr);
-      console.log($userSession);
-    })
-  })
-  
+      .get("/api/user/status")
+      .then((userStatusRes) => {
+        userSession.set(true);
+        console.log(userStatusRes.data);
+      })
+      .catch((userStatusErr) => {
+        userSession.set(false);
+        console.log(userStatusErr);
+        console.log($userSession);
+      });
+  });
+
   function redirectToLogin() {
     window.location.href = "/login";
   }
 </script>
-
-<head>
-  <title>PalitasPR | Login To Continue</title>
-</head>
 
 <div
   class="flex flex-col items-center justify-center h-full max-w-md min-h-screen p-6 m-auto text-teal-800 rounded-md sm:p-10"
@@ -34,7 +30,9 @@
     <h1 class="my-3 text-4xl font-bold text-[#cc2936]">
       Please Login to Continue
     </h1>
-    <p class="text-lg text-[#1f1f1f]">You need to log in to access this page.</p>
+    <p class="text-lg text-[#1f1f1f]">
+      You need to log in to access this page.
+    </p>
   </div>
   <div class="space-y-2">
     <button
