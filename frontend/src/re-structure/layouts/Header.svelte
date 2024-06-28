6bb35e17c8389422c60eccc7f0c20108fcc5662a
prodjohnper <JonathanPerez9743@gmail.com>
2024-06-28 01:00:40 -0400

    Update - New color scheme implementation | First iteration

---
+++ frontend/src/re-structure/layouts/Header.svelte
@@ -0,0 +1,51 @@
+<!-- Header Layout -->
+<script>
+  import { link } from "svelte-routing";
+  import { userSession } from "../../scripts/stores";
+
+  // Function to handle the "PalitasPR" link click
+  function handlePalitasPRClick(event) {
+    event.preventDefault();
+    if (window.location.pathname === "/") {
+      window.location.reload();
+    } else {
+      window.location.href = "/";
+    }
+  }
+</script>
+
+<!-- Header Start -->
+<header
+  class={$userSession
+    ? `hidden`
+    : `` + " p-2 md:p-4 shadow-lg md:shadow-xl navbar bg-primary "}
+>
+  <div class="navbar-start">
+    <a
+      use:link
+      on:click={handlePalitasPRClick}
+      href="/"
+      rel="noopener noreferrer"
+      class="text-xl bg-transparent border-none hover:from-[#1f1f1f] hover:scale-110 transition-transform ease-in-out duration-200 mx-4 rounded-full"
+    >
+      <img class="w-24 md:w-28" src="/logoDark.svg" alt="" />
+    </a>
+  </div>
+  <div class="gap-2 mr-4 navbar-end">
+    <a
+      use:link
+      href="/login"
+      rel="noopener noreferrer"
+      class="btn w-16 md:w-24 shadow-lg text-[#f1f1f1] bg-accent hover:bg-green-900"
+      >Login</a
+    >
+    <a
+      use:link
+      href="/signup"
+      rel="noopener noreferrer"
+      class="w-16 font-semibold border-2 shadow-lg md:w-24 text-accent border-accent btn bg-primary hover:opacity-80 hover:border-accent"
+      >Signup</a
+    >
+  </div>
+</header>
+<!-- Header End -->
