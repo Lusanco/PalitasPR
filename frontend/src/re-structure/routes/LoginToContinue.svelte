6c527b260f666e651dd9cd39a4c35a7a1f51de20
prodjohnper <JonathanPerez9743@gmail.com>
2024-06-28 01:02:50 -0400

    Update - New color scheme implementation | First iteration

---
+++ frontend/src/re-structure/routes/LoginToContinue.svelte
@@ -0,0 +1,46 @@
+<!-- Alfre remember rename/move correctly this component -->
+<script>
+  import { onMount } from "svelte";
+  import { userSession } from "../../scripts/stores";
+  import axios from "axios";
+
+  onMount(() => {
+    axios
+      .get("/api/user/status")
+      .then((userStatusRes) => {
+        userSession.set(true);
+        console.log(userStatusRes.data);
+      })
+      .catch((userStatusErr) => {
+        userSession.set(false);
+        console.log(userStatusErr);
+        console.log($userSession);
+      });
+  });
+
+  function redirectToLogin() {
+    window.location.href = "/login";
+  }
+</script>
+
+<div
+  class="flex flex-col items-center justify-center h-full max-w-md min-h-screen p-6 m-auto text-teal-800 rounded-md sm:p-10"
+>
+  <div class="mb-8 text-center">
+    <h1 class="my-3 text-4xl font-bold text-[#cc2936]">
+      Please Login to Continue
+    </h1>
+    <p class="text-lg text-[#1f1f1f]">
+      You need to log in to access this page.
+    </p>
+  </div>
+  <div class="space-y-2">
+    <button
+      on:click={redirectToLogin}
+      type="button"
+      class="bg-[#cc2936] text-[#f1f1f1] hover:text-[#1f1f1f] hover:bg-white btn px-6"
+    >
+      Login
+    </button>
+  </div>
+</div>
