9637e27a27e97a2e63dbc63244db52a23bde7058
prodjohnper <JonathanPerez9743@gmail.com>
2024-06-27 21:13:26 -0400

    Update - tab dynamic title implementation

--- frontend/src/routes/ServiceDetails.svelte
+++ frontend/src/routes/ServiceDetails.svelte
@@ -30,7 +30,7 @@
   let urlArr;
   let initialContact;
 
-  let isLoading = writable(true)
+  let isLoading = writable(true);
 
   onMount(() => {
     console.log("ServiceDetails Component Has Mounted");
@@ -77,155 +77,149 @@
   });
 </script>
 
-<head>
-  <title>PalitasPR | Service Details</title>
-</head>
-
 {#if $isLoading}
-<Loading />
-{:else}
-  {#if $response1 && $response2 && $profileID}
-    <!-- ServiceDetails Container -->
+  <Loading />
+{:else if $response1 && $response2 && $profileID}
+  <!-- ServiceDetails Container -->
+  <div
+    class="flex flex-col items-center justify-center w-full h-full min-h-screen py-20 m-auto bg-[#f1f1f1]"
+  >
+    <!-- ServiceDetails Flex Wrap -->
     <div
-      class="flex flex-col items-center justify-center w-full h-full min-h-screen py-20 m-auto bg-[#f1f1f1]"
+      class="flex flex-wrap items-center justify-center w-full h-full m-auto bg-white md:max-w-6xl join min-h-96"
     >
-      <!-- ServiceDetails Flex Wrap -->
+      <!-- ServiceDetails Left -->
       <div
-        class="flex flex-wrap items-center justify-center w-full h-full m-auto bg-white md:max-w-6xl join min-h-96"
+        class="h-full gap-1 p-4 element overflow-y-auto rounded-none card md:border-r-2 min-h-96 md:border-[#cc2936] basis-full md:w-fit md:basis-1/2 bg-white"
       >
-        <!-- ServiceDetails Left -->
+        <br />
+        <h1 class="self-center text-3xl card-title text-[#1f1f1f]">
+          {$response1.results.title}
+        </h1>
+        <br />
         <div
-          class="h-full gap-1 p-4 element overflow-y-auto rounded-none card md:border-r-2 min-h-96 md:border-[#cc2936] basis-full md:w-fit md:basis-1/2 bg-white"
+          class="flex flex-col overflow-hidden overflow-y-scroll min-h-96 h-96 element text-[#1f1f1f]"
         >
-          <br />
-          <h1 class="self-center text-3xl card-title text-[#1f1f1f]">
-            {$response1.results.title}
-          </h1>
-          <br />
-          <div
-            class="flex flex-col overflow-hidden overflow-y-scroll min-h-96 h-96 element text-[#1f1f1f]"
-          >
-            {#if !$response1.results.pictures || $response1.results.pictures.length === 0}
-              <div
-                class="self-center w-full h-40 rounded-none skeleton min-h-40"
-              ></div>
-            {:else}
-              <div
-                class="self-center object-cover w-full h-40 rounded-none max-h-40 min-h-40"
-              >
-                <img
-                  class="self-center object-cover w-full h-40 rounded-none max-h-40"
-                  src={$response1.results.pictures[0]}
-                  alt=""
-                />
-              </div>
-            {/if}
-            <br />
-            <div class="flex flex-wrap justify-between text-xl">
-              <span>
-                {$response1.results.first_name}
-                {$response1.results.last_name}
-              </span>
-              <span>{Object.keys(servicesID)[$response1.results.service_id]}</span
-              >
+          {#if !$response1.results.pictures || $response1.results.pictures.length === 0}
+            <div
+              class="self-center w-full h-40 rounded-none skeleton min-h-40"
+            ></div>
+          {:else}
+            <div
+              class="self-center object-cover w-full h-40 rounded-none max-h-40 min-h-40"
+            >
+              <img
+                class="self-center object-cover w-full h-40 rounded-none max-h-40"
+                src={$response1.results.pictures[0]}
+                alt=""
+              />
             </div>
-            <!-- <br> -->
-            <hr class="border-[#cc2936]" />
-            <div class="mt-2">
-              Pueblos Disponibles: {#each $response1.results.towns as town}
-                <span class="mx-1">{town},</span>
-              {/each}
-            </div>
-            <!-- <hr class="border-[#cc2936]" /> -->
-            <h3 class="mt-5 text-xl">Descripción</h3>
-            <hr class="border-[#cc2936]" />
-            <p class="self-center w-full mt-2 text-justify min-h-40">
-              {$response1.results.description}
-            </p>
+          {/if}
+          <br />
+          <div class="flex flex-wrap justify-between text-xl">
+            <span>
+              {$response1.results.first_name}
+              {$response1.results.last_name}
+            </span>
+            <span>{Object.keys(servicesID)[$response1.results.service_id]}</span
+            >
           </div>
-          <br />
+          <!-- <br> -->
+          <hr class="border-[#cc2936]" />
+          <div class="mt-2">
+            Pueblos Disponibles: {#each $response1.results.towns as town}
+              <span class="mx-1">{town},</span>
+            {/each}
+          </div>
+          <!-- <hr class="border-[#cc2936]" /> -->
+          <h3 class="mt-5 text-xl">Descripción</h3>
+          <hr class="border-[#cc2936]" />
+          <p class="self-center w-full mt-2 text-justify min-h-40">
+            {$response1.results.description}
+          </p>
         </div>
-        <!-- Left -->
+        <br />
+      </div>
+      <!-- Left -->
 
-        <!-- ServiceDetails Right -->
+      <!-- ServiceDetails Right -->
+      <div
+        class="flex flex-col h-full gap-1 p-4 bg-white rounded-none card min-h-96 basis-full md:w-fit md:basis-1/2"
+      >
+        <br />
+        <h1 class="self-center text-3xl card-title text-[#1f1f1f]">Reviews</h1>
+        <br />
+
         <div
-          class="flex flex-col h-full gap-1 p-4 bg-white rounded-none card min-h-96 basis-full md:w-fit md:basis-1/2"
+          class="flex flex-col gap-2 overflow-hidden overflow-y-scroll element min-h-96 h-96"
         >
-          <br />
-          <h1 class="self-center text-3xl card-title text-[#1f1f1f]">Reviews</h1>
-          <br />
-
-          <div
-            class="flex flex-col gap-2 overflow-hidden overflow-y-scroll element min-h-96 h-96"
-          >
-            {#if !$response2.results || $response2.results.length === 0}
+          {#if !$response2.results || $response2.results.length === 0}
+            <div
+              class="font-bold text-xl flex flex-col justify-center items-center text-[#cc2936] text-center h-full w-full"
+            >
+              No Reviews Yet
+            </div>
+          {:else}
+            {#each $response2.results as review}
               <div
-                class="font-bold text-xl flex flex-col justify-center items-center text-[#cc2936] text-center h-full w-full"
+                class="flex flex-col justify-between p-4 shadow-md bg-[#f1f1f1] card rounded-lg"
               >
-                No Reviews Yet
-              </div>
-            {:else}
-              {#each $response2.results as review}
+                <div class="flex justify-between gap-2 text-[#1f1f1f]">
+                  <div>
+                    {`${review.first_name} ${review.last_name}`}
+                  </div>
+                  <div>{`${review.rating}/5.0`}</div>
+                </div>
+                <br />
                 <div
-                  class="flex flex-col justify-between p-4 shadow-md bg-[#f1f1f1] card rounded-lg"
+                  class="h-full py-6 text-justify line-clamp-none overflow-ellipsis text-[#1f1f1f]"
                 >
-                  <div class="flex justify-between gap-2 text-[#1f1f1f]">
-                    <div>
-                      {`${review.first_name} ${review.last_name}`}
-                    </div>
-                    <div>{`${review.rating}/5.0`}</div>
-                  </div>
-                  <br />
-                  <div
-                    class="h-full py-6 text-justify line-clamp-none overflow-ellipsis text-[#1f1f1f]"
-                  >
-                    {review.description}
-                  </div>
-                  <div class="flex justify-between gap-2 mt-8 text-[#1f1f1f]">
-                    <div>{review.created_at}</div>
-                    <!-- <button
+                  {review.description}
+                </div>
+                <div class="flex justify-between gap-2 mt-8 text-[#1f1f1f]">
+                  <div>{review.created_at}</div>
+                  <!-- <button
                       class="absolute btn right-4 bottom-4 bg-[#cc2936] text-white hover:bg-white hover:text-[#1f1f1f] hover:shadow-md"
                       >Images</button
                     > -->
-                  </div>
                 </div>
-              {/each}
-            {/if}
-          </div>
-          <br />
-          <!-- Flex Wrap -->
+              </div>
+            {/each}
+          {/if}
+        </div>
+        <br />
+        <!-- Flex Wrap -->
+        <div
+          class="flex items-center justify-center w-full bg-white h-fit md:max-w-6xl"
+        >
           <div
-            class="flex items-center justify-center w-full bg-white h-fit md:max-w-6xl"
+            class="flex flex-col items-center justify-center w-10/12 gap-4 mx-4 bg-white md:flex-row md:w-11/12 md:max-w-6xl"
           >
-            <div
-              class="flex flex-col items-center justify-center w-10/12 gap-4 mx-4 bg-white md:flex-row md:w-11/12 md:max-w-6xl"
+            <Link
+              class="md:w-1/3 w-full btn px-8 py-3 font-semibold bg-[#cc2936] text-[#f1f1f1] rounded hover:bg-white hover:text-[#1f1f1f] hover:shadow-md flex flex-col justify-center items-center"
+              to="/">Back To Search</Link
             >
+            {#if $userSession === false}
               <Link
                 class="md:w-1/3 w-full btn px-8 py-3 font-semibold bg-[#cc2936] text-[#f1f1f1] rounded hover:bg-white hover:text-[#1f1f1f] hover:shadow-md flex flex-col justify-center items-center"
-                to="/">Back To Search</Link
+                to="/login-to-continue">Go To Profile</Link
               >
-              {#if $userSession === false}
-                <Link
-                  class="md:w-1/3 w-full btn px-8 py-3 font-semibold bg-[#cc2936] text-[#f1f1f1] rounded hover:bg-white hover:text-[#1f1f1f] hover:shadow-md flex flex-col justify-center items-center"
-                  to="/login-to-continue">Go To Profile</Link
-                >
-                <Link
-                  class="md:w-1/3 w-full btn px-8 py-3 font-semibold bg-[#cc2936] text-[#f1f1f1] rounded hover:bg-white hover:text-[#1f1f1f] hover:shadow-md flex flex-col justify-center items-center"
-                  to="/login-to-continue">Send Contact</Link
-                >
-              {:else}
-                <Link
-                  class="md:w-1/3 w-full btn px-8 py-3 font-semibold bg-[#cc2936] text-[#f1f1f1] rounded hover:bg-white hover:text-[#1f1f1f] hover:shadow-md flex flex-col justify-center items-center"
-                  to={`/profile/${$profileID}`}>Go To Profile</Link
-                >
-                <Button {image} {button} />
-              {/if}
-            </div>
+              <Link
+                class="md:w-1/3 w-full btn px-8 py-3 font-semibold bg-[#cc2936] text-[#f1f1f1] rounded hover:bg-white hover:text-[#1f1f1f] hover:shadow-md flex flex-col justify-center items-center"
+                to="/login-to-continue">Send Contact</Link
+              >
+            {:else}
+              <Link
+                class="md:w-1/3 w-full btn px-8 py-3 font-semibold bg-[#cc2936] text-[#f1f1f1] rounded hover:bg-white hover:text-[#1f1f1f] hover:shadow-md flex flex-col justify-center items-center"
+                to={`/profile/${$profileID}`}>Go To Profile</Link
+              >
+              <Button {image} {button} />
+            {/if}
           </div>
         </div>
-        <!-- Right -->
       </div>
+      <!-- Right -->
     </div>
-    <!-- Container -->
-  {/if}
+  </div>
+  <!-- Container -->
 {/if}
