52be627ee1737ede367ea910e5768ab89018c52a
prodjohnper <JonathanPerez9743@gmail.com>
2024-06-28 01:00:18 -0400

    Update - New color scheme implementation | First iteration

---
+++ frontend/src/re-structure/components/Slogan.svelte
@@ -0,0 +1,12 @@
+<!-- LogoSlogan Start -->
+<div class="flex flex-col justify-center mx-auto text-center">
+  <!-- <div class="absolute bg-white w-96"></div> -->
+  <img class="px-4 w-96" src="/logoDark.svg" alt="" />
+  <!-- <h1 class="text-6xl font-bold">
+    Palitas<span class="text-stone-700">PR</span>
+  </h1> -->
+  <p class="text-xl italic font-medium text-accent">
+    Servicios Locales y Accesibles
+  </p>
+</div>
+<!-- LogoSlogan End -->
