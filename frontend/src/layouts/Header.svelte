8cf492cdc2e4dcda369cffd4ab094cc02ae0a79e
Lusanco <lasc1026@gmail.com>
2024-06-29 20:25:23 -0400

    fix - backbutton and burger menu in wrong placement

--- frontend/src/layouts/Header.svelte
+++ frontend/src/layouts/Header.svelte
@@ -1,7 +1,22 @@
 <!-- Header Layout -->
 <script>
   import { link } from "svelte-routing";
+  import Menu from "../components/Menu.svelte";
+  import Button from "../components/Button.svelte";
   import { userSession } from "../scripts/stores";
+
+  // Button Prop Variables And Dependencies
+  let image = null;
+  let button = {
+    name: "",
+    method: "",
+    url: "",
+    headers: "",
+    twcss:
+      "flex flex-col text-xl font-semibold absolute items-center justify-center shadow-none w-20 h-20 btn btn-sm border-none text-secondary rounded-2xl hover:bg-accent/50 hover:text-primary focus:outline-none focus:ring-0 m-2 z-50",
+    misc: { "App Location": "Back Button Component" },
+  };
+  // Button Prop Variables And Dependencies
 
   // Function to handle the "PalitasPR" link click
   function handlePalitasPRClick(event) {
@@ -51,4 +66,16 @@
     </div>
   </header>
   <!-- Header End -->
+{/if}
+{#if $userSession || !$userSession}
+  <div
+    class={`${$userSession ? "flex flex-row justify-between items-stretch w-full bg-transparent px-2" : "hidden"}`}
+  >
+    <Button {image} {button}>
+      <span class="sr-only">Go back</span>
+      <i class="text-md fa-solid fa-chevron-left"></i>
+    </Button>
+
+    <Menu></Menu>
+  </div>
 {/if}
